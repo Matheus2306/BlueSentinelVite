4a115bc3c3cd30235d2eac54ef665d6bef7a1cc8
limazxz <goubulsegue@gmail.com>
2025-09-30 16:55:16 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+        <img src="src/img/logoHeader.png" alt="Logo Blue Sentinel" id="logo" />
+    </div>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "react-router"
-import './index.css'
 import App from './App.jsx'
-import { Route } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
