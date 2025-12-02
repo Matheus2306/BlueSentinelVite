29a8128342d0bb599f851b67f1953a72c3c22008
matheus2306 <math2306.r@gmail.com>
2025-12-02 11:48:23 -0300

    feat: add NotFound page with 404 error message and styling; enhance scrollbar in CardBody

--- src/components/CardBody.jsx
+++ src/components/CardBody.jsx
@@ -3,12 +3,10 @@ import ItemCard from "./ItemCard";
 
 const CardBody = ({ data, handleDeleteDrone }) => {
   return (
-    <div className="d-flex flex-column h-75 gap-2 mt-2 overflow-y-scroll">
+    <div className="d-flex flex-column h-75 gap-2 mt-2 overflow-y-scroll scrollbar">
       {data.map((drone) => (
           <ItemCard modelo={drone.droneFabri.modelo} localizacao={drone.localizacao} tempoAtividade={drone.tempoEmHoras} status={drone.status} Id={drone.droneId} handleDeleteDrone={handleDeleteDrone} />
       ))
-
-      
       }
     </div>
   );
